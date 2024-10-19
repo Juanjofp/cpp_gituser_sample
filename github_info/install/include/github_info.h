6142abe30a263950a902fa2dc495a60df10ebe3c
juanjo <juanjo@juanjofp.com>
2024-10-19 10:59:25 +0200

    [WIP] Repositories

---
+++ github_info/install/include/github_info.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <optional>
+#include <string>
+
+#include "githubrepository.h"
+#include "githubuser.h"
+
+namespace jjfp::github_info {
+
+class GithubInfo {
+ public:
+  virtual std::optional<GitUser> user() const = 0;
+  virtual std::optional<GitRepository> repositories() = 0;
+
+  virtual std::string print_version() = 0;
+};
+
+}  // namespace jjfp::github_info
